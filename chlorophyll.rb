bfbfdde3d82e43c1d2fc4ab0d48e2ddf26f6e97a
Efseykho <mturovskiy@gmail.com>
2012-05-10 15:05:50 -0400

    code brute-force absorbance search. fix bug re: absorbances being allowed < 0

--- chlorophyll.rb
+++ chlorophyll.rb
@@ -31,15 +31,69 @@ class Chlorophyll
      
      file.readline
      file.each_line do |line| 
-       line = line.strip.split('  ')
+       line = line.strip.split(/\s+/)
        @abs[line[0].to_f] = line[1].to_f
      end
     
      file.close
-     
-       
+  end
   
+  #for now this performs brute-force absorbance search
+  #it should really be binary-search
+  def find_absorbance(abs_val)
+    return 0.0 if @abs.keys[0] > abs_val #lower-bound of absorption
+    return 0.0 if abs_val > @abs.keys[-1] #upper-bound of absorption
+    
+    @abs.keys.each_index do |i|
+       #exact match
+       return @abs[@abs.keys[i]]  if @abs.keys[i] == abs_val
+       
+       #weighted average of distance between points
+       if @abs.keys[i] < abs_val && @abs.keys[i+1] > abs_val
+         
+         #point_1 = xi, point_2 = xj, distance = x, d1+d2=x
+         #mean_weight = xi*d2/x + x2*d1/x
+         distance =  @abs.keys[i+1] - @abs.keys[i]
+         ##puts('distance ' +distance.to_s)
+         d1 = (abs_val - @abs.keys[i])*1.0/distance
+         d2 = (@abs.keys[i+1] - abs_val)*1.0/distance
+         ##puts('d1 ' +d1.to_s)
+         ##puts('d2 ' +d2.to_s)
+         
+         return   @abs[@abs.keys[i]]*d2 +   @abs[@abs.keys[i+1]]*d1
+       end
+    end
+    
   end
   
   
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
